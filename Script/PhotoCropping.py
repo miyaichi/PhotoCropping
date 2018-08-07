cb8423a03f983575ff869d60dec883e6c42c6b8d
Yoshihiko Miyaichi <miyaichi@gmail.com>
2018-08-07 12:54:24 +0300

    First CheckIn

---
+++ Script/PhotoCropping.py
@@ -0,0 +1,53 @@
+import cv2
+
+(width, height) = (180, 225)  # 切り出す画像のサイズ（pixel）
+face_retio = 0.6  # 顔の高さの割合
+top_ratio = 0.1  # 顔の上のスペースの割合
+
+
+def expand(rect, scale):
+    (x, y, w, h) = rect
+    y = y - int(h * scale / 100)
+    h = h + int(h * scale / 100)
+    return (x, y, w, h)
+
+
+def facedetect(gray):
+    cascade = cv2.CascadeClassifier('Script\haarcascade_frontalface_alt.xml')
+    rects = cascade.detectMultiScale(gray, 1.11, 5)
+    if len(rects) > 0:
+        return rects[0]
+    return None
+
+
+def crop(before, after, scale=20):
+    image = cv2.imread(before)
+
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+    # 顔認識
+    face = facedetect(gray)
+    if face is None:
+        # Can not detect face.
+        return False
+
+    # 髪の毛の部分をscaleパラメータを使って拡張
+    (fx, fy, fw, fh) = expand(face, scale)
+
+    # 位置決め
+    h = int(fh / face_retio)
+    y = fy - int(h * top_ratio)
+    w = int(h / height * width)
+    x = fx + int(fw / 2) - int(w / 2)
+
+    # トリミング
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    photo = rgb[y:y + h, x:x + w]
+    photo = cv2.resize(photo, (width, height))
+
+    # 切り抜いた画像の書き出し
+    photo = cv2.cvtColor(photo, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(after, photo)
+
+    return True
